6a8ee3faf46ecbe431cd6ee51fae67bbcfcce849
aleksey.krasnov <krasnov.80@mail.ru>
2023-06-09 12:08:19 +0300

    Определить индексы

--- task32.py
+++ task32.py
@@ -8,3 +8,21 @@
 0, -5, -5, 7]
 Вывод: [1, 9, 13, 14, 19]
 '''
+
+from random import randint
+
+min_num=int(input('Введите минимальное значение '))
+max_num=int(input('Введите максимальное значение '))
+
+
+my_list=[randint(-10,10) for _ in range(20)]
+res_list=list()
+
+for i in range(len(my_list)):
+    if my_list[i]>min_num and my_list[i]<max_num:
+        res_list.append(i)
+        
+
+print(my_list)
+print(res_list)
+
